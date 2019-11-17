a4bc67ea99c1974978e4e6c39671a9298916edab
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-17 12:38:51 +0530

    supplier post and get done

--- POS.Repositories/IUnitOfWork.cs
+++ POS.Repositories/IUnitOfWork.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using POS.Repositories.Addresses;
-using POS.Repositories.Communications;
-using POS.Repositories.Collaborators;
 using POS.Repositories.Suppliers;
 using POS.Core.DI;
 
@@ -14,9 +11,6 @@ namespace POS.Repositories
     [AutoDIService(implementationType: "POS.Repositories.UnitOfWork")]
     public interface IUnitOfWork : IDisposable
     {
-        IAddressRepository Addresses { get; }
-        ICommunicationRepository Communications { get; }
-        ICollaboratorRepository Collaborators { get; }
         ISupplierRepository Suppliers { get; }
         Task<int> Complete();
     }
--- POS.Repositories/UnitOfWork.cs
+++ POS.Repositories/UnitOfWork.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using POS.DAL;
-using POS.Repositories.Addresses;
-using POS.Repositories.Communications;
-using POS.Repositories.Collaborators;
 using POS.Repositories.Suppliers;
 
 namespace POS.Repositories
@@ -13,23 +10,14 @@ namespace POS.Repositories
     public class UnitOfWork : IUnitOfWork
     {
         private readonly DataBaseContext context;
-        public IAddressRepository Addresses { get; set; }
-        public ICommunicationRepository Communications { get; set; }
-        public ICollaboratorRepository Collaborators { get; set; }
 
         public ISupplierRepository Suppliers { get; set; }
 
 
         public UnitOfWork(DataBaseContext context,
-            IAddressRepository addressRepository,
-            ICollaboratorRepository collaboratorRepository,
-            ICommunicationRepository communicationRepository,
             ISupplierRepository supplierRepository)
         {
             this.context = context;
-            Addresses = addressRepository;
-            Communications = communicationRepository;
-            Collaborators = collaboratorRepository;
             Suppliers = supplierRepository;
         }
 
