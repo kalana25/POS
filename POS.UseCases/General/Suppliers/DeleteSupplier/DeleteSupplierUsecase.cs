8314d8b75f8952adbcb70306fd1147721895501b
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-22 17:45:25 +0530

    delete supplier usecase

---
+++ POS.UseCases/General/Suppliers/DeleteSupplier/DeleteSupplierUsecase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using POS.Repositories;
+using POS.UseCases.DTO;
+using POS.Models;
+using POS.Core.Interfaces;
+
+namespace POS.UseCases.General.Suppliers.DeleteSupplier
+{
+    public class DeleteSupplierUsecase: UseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public int Id { get; set; }
+
+        public DeleteSupplierUsecase(
+            IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int>Execute()
+        {
+            Supplier supplier = await unitOfWork.Suppliers.Get(Id);
+            unitOfWork.Suppliers.Remove(supplier);
+            return await unitOfWork.Complete();
+        }
+
+    }
+}
