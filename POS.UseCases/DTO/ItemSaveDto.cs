bb09533e89bdef4fb26f26850be295cd29003662
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-23 12:51:59 +0530

    Item save usecase

---
+++ POS.UseCases/DTO/ItemSaveDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace POS.UseCases.DTO
+{
+    public class ItemSaveDto
+    {
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Barcode { get; set; }
+    }
+}
---
+++ POS.UseCases/General/Items/SaveItem/SaveItemUsecase.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using POS.Core.Interfaces;
+using AutoMapper;
+using POS.Models;
+using POS.Repositories;
+using POS.UseCases.DTO;
+
+namespace POS.UseCases.General.Items.SaveItem
+{
+    public class SaveItemUsecase:UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ItemSaveDto Dto { get; set; }
+
+        public SaveItemUsecase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<Item> Execute()
+        {
+            Item item = mapper.Map<ItemSaveDto, Item>(Dto);
+            unitOfWork.Item.Add(item);
+            await unitOfWork.Complete();
+            return item;
+        }
+    }
+}
