0b0d9f93287666f1b3370f5b65910a8ef31d57bc
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-23 09:24:35 +0530

    get item category usecase

---
+++ POS.UseCases/General/ItemCategories/GetItemCategory/GetItemCategoryUsecase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using POS.Core.Interfaces;
+using AutoMapper;
+using POS.Models;
+using POS.Repositories;
+using POS.UseCases.DTO;
+
+namespace POS.UseCases.General.ItemCategories.GetItemCategory
+{
+    public class GetItemCategoryUsecase:UseCase
+    {
+        public int Id { get; set; }
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetItemCategoryUsecase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ItemCategoryInfoDto> Execute()
+        {
+            ItemCategory category = await unitOfWork.ItemCategory.GetItemCategory(Id);
+            ItemCategoryInfoDto result = mapper.Map<ItemCategory, ItemCategoryInfoDto>(category);
+            return result;
+        }
+    }
+}
