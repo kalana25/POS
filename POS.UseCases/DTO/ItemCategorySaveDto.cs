8f140f099d120913cb79cb4f64c9c6645a408c71
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-23 09:39:44 +0530

    item category save usecase

---
+++ POS.UseCases/DTO/ItemCategorySaveDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace POS.UseCases.DTO
+{
+    public class ItemCategorySaveDto
+    {
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public int ParentCategoryId { get; set; }
+
+        public int? Level { get; set; }
+    }
+}
---
+++ POS.UseCases/General/ItemCategories/SaveItemCategory/SaveItemCategoryUsecase.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using POS.Core.Interfaces;
+using AutoMapper;
+using POS.Models;
+using POS.Repositories;
+using POS.UseCases.DTO;
+
+namespace POS.UseCases.General.ItemCategories.SaveItemCategory
+{
+    public class SaveItemCategoryUsecase:UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public ItemCategorySaveDto Dto { get; set; }
+
+        public SaveItemCategoryUsecase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<ItemCategory> Execute()
+        {
+            ItemCategory itemCategory = mapper.Map<ItemCategorySaveDto, ItemCategory>(Dto);
+            unitOfWork.ItemCategory.Add(itemCategory);
+            await unitOfWork.Complete();
+            return itemCategory;
+        }
+
+
+    }
+}
