c8a55d081c8f722b0ff741d443992fe2f6111593
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2020-02-28 11:36:56 +0530

    restrict cascade delete.

--- POS.DAL/DataBaseContext.cs
+++ POS.DAL/DataBaseContext.cs
@@ -12,12 +12,44 @@ namespace POS.DAL
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Restict auto delete items when a category is deleted.
+            modelBuilder.Entity<ItemCategory>()
+                .HasMany(c => c.Items)
+                .WithOne(i => i.ItemCateogry)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Restict auto delete PurchaseOderDetails when a PurchaseOrder is deleted.
+            modelBuilder.Entity<PurchaseOrder>()
+                .HasMany(h => h.Items)
+                .WithOne(d => d.PurchaseOrder)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Restict auto delete GoodReceivedNoteItems when a GoodReceivedNote is deleted.
+            modelBuilder.Entity<GoodReceivedNote>()
+                .HasMany(h => h.Items)
+                .WithOne(d => d.GoodReceivedNote)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //Restict auto delete GoodReceivedNoteItems when a PurchaseOrderDetail is deleted.
+            modelBuilder.Entity<PurchaseOrderDetail>()
+                .HasMany(p => p.GoodReceivedNoteItems)
+                .WithOne(g => g.PurchaseOrderDetail)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Communication> Communications { get; set; }
         public DbSet<Supplier> Suppliers { get; set; }
         public DbSet<ItemCategory> Categories { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+        public DbSet<PurchaseOrderDetail> PurchaseOrderDetails { get; set; }
+        
 
     }
 }
