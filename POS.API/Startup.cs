1dbab857bc684d64fda673728a2e61404e1f695b
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2020-03-30 20:29:51 +0530

    required this when releasing.

--- POS.API/Startup.cs
+++ POS.API/Startup.cs
@@ -74,6 +74,7 @@ namespace POS.API
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                //c.SwaggerEndpoint("/PosApi/swagger/v1/swagger.json", "API v1");
                 c.RoutePrefix = string.Empty;
             });
             app.UseMvc();
