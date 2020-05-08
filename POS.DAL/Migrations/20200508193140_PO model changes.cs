73d69354ffccf79e2970e958a84c6def0269af28
RKalana <kal25mahaarachchi@gmail.com>
2020-05-09 01:02:56 +0530

    db migraions

---
+++ POS.DAL/Migrations/20200508193140_PO model changes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace POS.DAL.Migrations
+{
+    public partial class POmodelchanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedBy",
+                table: "PurchaseOrders",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "PurchaseOrders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "PurchaseOrders",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "PurchaseOrders");
+        }
+    }
+}
