84b1a88c0c1fe18a1fbd44012d9cfdb80728b13c
RKalana <kal25mahaarachchi@gmail.com>
2020-05-11 19:03:57 +0530

    Authe controller,User register usecase and Dto

---
+++ POS.UseCases/DTO/UserRegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.UseCases.DTO
+{
+    public class UserRegisterDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
---
+++ POS.UseCases/General/Authentication/RegisterUser/RegisterUserUsecase.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using POS.Core.Interfaces;
+using AutoMapper;
+using POS.Models;
+using POS.Repositories;
+using POS.UseCases.DTO;
+namespace POS.UseCases.General.Authentication.RegisterUser
+{
+    public class RegisterUserUsecase:UseCase
+    {
+        public UserRegisterDto Dto { get; set; }
+
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public RegisterUserUsecase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        //public async Task<object> Execute()
+        //{
+
+        //}
+    }
+}
