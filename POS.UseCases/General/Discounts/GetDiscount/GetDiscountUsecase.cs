c716d8b2ccbe23c11ef4932803a2ac7a5a3cf0d6
RKalana <kal25mahaarachchi@gmail.com>
2020-05-27 23:12:22 +0530

    get discount usecase

---
+++ POS.UseCases/General/Discounts/GetDiscount/GetDiscountUsecase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using POS.Repositories;
+using POS.UseCases.DTO.Supplier;
+using POS.Models;
+using POS.Core.Interfaces;
+using POS.UseCases.DTO;
+
+namespace POS.UseCases.General.Discounts.GetDiscount
+{
+    public class GetDiscountUsecase: UseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public int Id { get; set; }
+
+        public GetDiscountUsecase(
+            IUnitOfWork unitOfWork,
+            IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+
+        public async Task<DiscountInfoDto> Execute()
+        {
+            var discount = await unitOfWork.Discounts.Get(Id);
+            DiscountInfoDto result = mapper.Map<Discount, DiscountInfoDto>(discount);
+            return result;
+        }
+    }
+}
