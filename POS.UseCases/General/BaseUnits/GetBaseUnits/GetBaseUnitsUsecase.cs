7077f05bd8149dcd80150b0eec5e9ea07d819880
RKalana <kal25mahaarachchi@gmail.com>
2020-06-03 00:01:37 +0530

    Get base units usecase

---
+++ POS.UseCases/General/BaseUnits/GetBaseUnits/GetBaseUnitsUsecase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using POS.Core.Interfaces;
+using AutoMapper;
+using POS.Models;
+using POS.Repositories;
+using POS.UseCases.DTO;
+
+namespace POS.UseCases.General.BaseUnits.GetBaseUnits
+{
+    public class GetBaseUnitsUsecase:UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetBaseUnitsUsecase(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<BaseUnitInfoDto>> Execute()
+        {
+            IEnumerable<BaseUnit> baseUnits = await unitOfWork.BaseUnits.GetAll();
+            IEnumerable<BaseUnitInfoDto> result = mapper.Map<IEnumerable<BaseUnit>, IEnumerable<BaseUnitInfoDto>>(baseUnits);
+            return result;
+        }
+    }
+}
