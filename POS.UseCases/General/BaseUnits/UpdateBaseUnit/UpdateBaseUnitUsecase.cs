bf4d998104557d85c4de2c1777e9cb4de429177b
RKalana <kal25mahaarachchi@gmail.com>
2020-06-03 00:13:54 +0530

    base unit update usecase

---
+++ POS.UseCases/General/BaseUnits/UpdateBaseUnit/UpdateBaseUnitUsecase.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using POS.Repositories;
+using POS.UseCases.DTO;
+using POS.Models;
+using POS.Core.Interfaces;
+
+namespace POS.UseCases.General.BaseUnits.UpdateBaseUnit
+{
+    public class UpdateBaseUnitUsecase:UseCase
+    {
+        private readonly IMapper mapper;
+        private readonly IUnitOfWork unitOfWork;
+
+        public int Id { get; set; }
+        public BaseUnitSaveDto Dto { get; set; }
+
+        public UpdateBaseUnitUsecase(
+            IMapper mapper,
+            IUnitOfWork unitOfWork)
+        {
+            this.mapper = mapper;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Execute()
+        {
+            BaseUnit unit = await unitOfWork.BaseUnits.Get(Id);
+            mapper.Map<BaseUnitSaveDto, BaseUnit>(Dto, unit);
+            return await unitOfWork.Complete();
+        }
+    }
+}
