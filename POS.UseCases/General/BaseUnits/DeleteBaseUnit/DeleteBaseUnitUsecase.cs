c485578bb5504b187350ed017350bef0b049f848
RKalana <kal25mahaarachchi@gmail.com>
2020-06-03 00:04:10 +0530

    delete base unit usecase

---
+++ POS.UseCases/General/BaseUnits/DeleteBaseUnit/DeleteBaseUnitUsecase.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using POS.Repositories;
+using POS.Models;
+using POS.Core.Interfaces;
+
+namespace POS.UseCases.General.BaseUnits.DeleteBaseUnit
+{
+    public class DeleteBaseUnitUsecase:UseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public int Id { get; set; }
+
+        public DeleteBaseUnitUsecase(
+            IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Execute()
+        {
+            BaseUnit unit = await unitOfWork.BaseUnits.Get(Id);
+            unitOfWork.BaseUnits.Remove(unit);
+            return await unitOfWork.Complete();
+        }
+    }
+}
