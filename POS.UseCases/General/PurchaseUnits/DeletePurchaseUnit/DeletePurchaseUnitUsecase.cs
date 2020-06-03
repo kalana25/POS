19cdf4c4b8e312a0630208f57fabd41863650af4
RKalana <kal25mahaarachchi@gmail.com>
2020-06-03 20:55:31 +0530

    Delete Purchase Units

---
+++ POS.UseCases/General/PurchaseUnits/DeletePurchaseUnit/DeletePurchaseUnitUsecase.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using POS.Repositories;
+using POS.Models;
+using POS.Core.Interfaces;
+
+namespace POS.UseCases.General.PurchaseUnits.DeletePurchaseUnit
+{
+    public class DeletePurchaseUnitUsecase:UseCase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public int Id { get; set; }
+
+        public DeletePurchaseUnitUsecase(
+            IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Execute()
+        {
+            PurchaseUnit unit = await unitOfWork.PurchaseUnits.Get(Id);
+            unitOfWork.PurchaseUnits.Remove(unit);
+            return await unitOfWork.Complete();
+        }
+    }
+}
