7c735c3ee4598886031a5dff227be285311352d7
RKalana <kal25mahaarachchi@gmail.com>
2020-06-17 11:56:34 +0530

    po update completed

--- POS.UseCases/DTO/PoDetailInfoWithItemDto.cs
+++ POS.UseCases/DTO/PoDetailInfoWithItemDto.cs
@@ -10,7 +10,8 @@ namespace POS.UseCases.DTO
         public int Id { get; set; }
         public int ItemId { get; set; }
         public int Quantity { get; set; }
-        public int Unit { get; set; }
+        public int UnitId { get; set; }
+        public bool IsBaseUnit { get; set; }
         public decimal UnitPrice { get; set; }
 
         public ItemInfoDto Item { get; set; }
