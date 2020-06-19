63396b5b6d32ffd23a1d07ecd2cead2197489f75
RKalana <kal25mahaarachchi@gmail.com>
2020-06-20 00:35:07 +0530

    inventory repository

---
+++ POS.Repositories/Inventories/InventoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using POS.DAL;
+using POS.Models;
+using POS.Core.General;
+namespace POS.Repositories.Inventories
+{
+    public class InventoryRepository:Repository<Inventory>,IInventoryRepository
+    {
+        public InventoryRepository(DataBaseContext context) : base(context)
+        {
+
+        }
+
+        public DataBaseContext DatabaseContext
+        {
+            get { return context as DataBaseContext; }
+        }
+    }
+}
