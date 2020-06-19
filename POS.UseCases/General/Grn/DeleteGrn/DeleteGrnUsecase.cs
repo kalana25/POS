804421b6364d9726f42833fd747c5ce7e5f45294
RKalana <kal25mahaarachchi@gmail.com>
2020-06-19 21:29:26 +0530

    empty uscase class

---
+++ POS.UseCases/General/Grn/DeleteGrn/DeleteGrnUsecase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.UseCases.General.Grn.DeleteGrn
+{
+    class DeleteGrnUsecase
+    {
+    }
+}
---
+++ POS.UseCases/General/Grn/GetGrn/GetGrnUsecase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.UseCases.General.Grn.GetGrn
+{
+    class GetGrnUsecase
+    {
+    }
+}
