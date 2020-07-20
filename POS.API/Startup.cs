60ab2c418c6a861b37c6c072faf8ba11ced5f13a
RKalana <kal25mahaarachchi@gmail.com>
2020-07-20 16:11:13 +0530

    Issue fix - polymorphic data binding

--- POS.API/Startup.cs
+++ POS.API/Startup.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
-using Swashbuckle.AspNetCore.Swagger;
 using POS.DAL;
 using POS.Core.General;
-using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using POS.Core.DI;
 using Microsoft.Extensions.Hosting;
-using Microsoft.CodeAnalysis.Options;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 
 namespace POS.API
 {
@@ -39,6 +32,8 @@ namespace POS.API
         {
             this.AppSettings = services.AddServiceCore(Configuration);
             services.AutoDIRegisterService();
+
+            services.AddControllers().AddNewtonsoftJson();
             var autoMapper = new AutoMapper.MapperConfiguration(c =>
             {
                 c.AddProfile(new MappingProfile());
@@ -139,8 +134,8 @@ namespace POS.API
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                //c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-                c.SwaggerEndpoint("/PosApi/swagger/v1/swagger.json", "API v1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+                //c.SwaggerEndpoint("/PosApi/swagger/v1/swagger.json", "API v1");
                 c.RoutePrefix = string.Empty;
             });
             app.UseMvc();
